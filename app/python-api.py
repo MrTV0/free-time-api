43d2cacd0a35ca32ebe53b545eef29f551d5630f
Taro Verschueren <57659923+MrTV0@users.noreply.github.com>
2022-11-04 17:40:38 +0100

    Update python-api.py

--- app/python-api.py
+++ app/python-api.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import platform
-import psutil
-import datetime
+from random import randint
 
 app = FastAPI()
 
@@ -21,53 +19,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/device/platform")
-async def get_platform():
-    return {'platform': platform.system(), 'datetime': datetime.datetime.now()}
-
-@app.get("/device/platform/{show_version}")
-async def get_platform_release(show_version: bool):
-    if(show_version):
-        return {'platform': platform.system(), 'release': platform.release(), 'version': platform.version(), 'datetime': datetime.datetime.now()}
-    return {'platform': platform.system(), 'release': platform.release(), 'datetime': datetime.datetime.now()}
-
-@app.get("/device/processor")
-async def get_processor():
-    return {'processor': platform.processor(), 'datetime': datetime.datetime.now()}
-
-@app.get("/device/interfaces/all")
-async def get_all_interfaces():
-    interfaces = []
-
-    # get all network interfaces (virtual and physical)
-    interface_addresses = psutil.net_if_addrs()
-    for interface_name in interface_addresses:
-        for address in interface_addresses[interface_name]:
-            if str(address.family) == 'AddressFamily.AF_INET':  # only IP addresses
-                interface = {}
-                interface["interface"] = interface_name
-                interface["IP address"] = address.address
-                interface["netmask"] = address.netmask
-                interfaces.append(interface)
-
-    return {'interfaces': interfaces, 'datetime': datetime.datetime.now()}
-
-@app.get("/device/interfaces/{index}")
-async def get_interface_by_index(index: int):
-    interfaces = []
-
-    # get all network interfaces (virtual and physical)
-    interface_addresses = psutil.net_if_addrs()
-    for interface_name in interface_addresses:
-        for address in interface_addresses[interface_name]:
-            if str(address.family) == 'AddressFamily.AF_INET':  # only IP addresses
-                interface = {}
-                interface["interface"] = interface_name
-                interface["IP address"] = address.address
-                interface["netmask"] = address.netmask
-                interfaces.append(interface)
-
-    if(index < 0 or index > len(interfaces)-1):
-        return {"error": "invalid index"}
-
-    return interfaces[index]
+@app.get("/percentage")
+async def get_random_percentage():
+    return {'percentage': randint(0, 100)}
