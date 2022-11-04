86ba86a1f7276acfe195a1424b9d79c90d4db6c1
Taro Verschueren <57659923+MrTV0@users.noreply.github.com>
2022-11-04 17:39:55 +0100

    Update python-api.py

--- app/python-api.py
+++ app/python-api.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import platform
 import psutil
 import datetime
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://localhost.tiangolo.com",
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/device/platform")
 async def get_platform():
