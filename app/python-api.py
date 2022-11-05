bcbdf075b8cd121f1495104052cf509fb605ecd3
Taro Verschueren <57659923+MrTV0@users.noreply.github.com>
2022-11-05 19:11:38 +0100

    Update python-api.py

--- app/python-api.py
+++ app/python-api.py
@@ -1,26 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.gzip import GZipMiddleware
-from random import randint
 
 app = FastAPI()
-
-origins = [
-    "https://mrtv0.github.io/website/",
-    "http://localhost",
-    "http://localhost:8080",
-    "https://localhost.tiangolo.com",
-    "http://127.0.0.1:5500"
-]
-
-app.add_middleware(GZipMiddleware)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
 
 @app.get("/percentage")
 async def get_random_percentage():
