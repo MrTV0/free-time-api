81d6fe1bc57997585499fa1fabfee529e1d45b8f
Taro Verschueren <57659923+MrTV0@users.noreply.github.com>
2022-11-08 14:27:31 +0100

    Update python-api.py

--- app/python-api.py
+++ app/python-api.py
@@ -37,4 +37,4 @@ async def get_random_percentage():
     #coffee_response = urlopen("https://coffee.alexflipnote.dev/random.json")
     #coffee_json = json.loads(coffee_response.read())
     #coffee = coffee.get("file")
-    return {"text": "Have a coffee!", "picture": "hello"}
+    return {"text": "Have a coffee!", "picture": "/hello"}
