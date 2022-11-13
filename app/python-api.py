7c050aede55ce8edde44760c17ffeb5c42cc9075
Taro Verschueren <57659923+MrTV0@users.noreply.github.com>
2022-11-13 17:03:48 +0100

    Update python-api.py

--- app/python-api.py
+++ app/python-api.py
@@ -19,19 +19,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#class Item(BaseModel):
-#    name: str
-#    description: str | None = None
-#    price: float
-#    tax: float | None = None
+class Bored(BaseModel):
+    activity_sel: str | None = None
+    participants_amount: int | None = None
 
-@app.get("/activity")
-async def get_random_percentage():
-    response = urlopen(activity_url)
-    activity_json = json.loads(response.read())
-    return {"activity": activity_json.get("activity"), "type": activity_json.get("type")}
+@app.post("/activity")
+async def create_item(bored: Bored):
+    if bored.activity_sel:
+        activity_urling = activity_url + "?type=" + bored.activity_sel
+        if bored.participants_amount and bored.participants_amount > 0:
+            activity_urlings = activity_urling + "?participants=" + str(bored.participants_amount)
+            response = urlopen(activity_urlings)
+            activity_json = json.loads(response.read())
+            return {"activity": activity_json.get("activity"), "type": activity_json.get("type"), "part_amount": activity_json.get("participants")}
+        else:
+            response = urlopen(activity_urling)
+            activity_json = json.loads(response.read())
+            return {"activity": activity_json.get("activity"), "type": activity_json.get("type"), "part_amount": activity_json.get("participants")}
+    if bored.participants_amount and bored.participants_amount > 0:
+        activity_urling = activity_url + "?participants=" + str(bored.participants_amount)
+        response = urlopen(activity_urling)
+        activity_json = json.loads(response.read())
+        return {"activity": activity_json.get("activity"), "type": activity_json.get("type"), "part_amount": activity_json.get("participants")}
+    else:
+        response = urlopen(activity_url)
+        activity_json = json.loads(response.read())
+        return {"activity": activity_json.get("activity"), "type": activity_json.get("type"), "part_amount": activity_json.get("participants")}
 
-@app.get("/choise")
+@app.get("/choice")
 async def get_random_percentage():
     response = urlopen(yesno_url)
     yesno_json = json.loads(response.read())
@@ -40,3 +55,11 @@ async def get_random_percentage():
 @app.get("/break")
 async def get_random_percentage():
     return {"text": "Have a coffee!", "picture": "https://coffee.alexflipnote.dev/HrMCgJMACXc_coffee.png"}
+
+@app.post("/bored/")
+async def create_item(bored: Bored):
+    if bored.activity_sel:
+        activity_urling = activity_url + "?type=" + bored.activity_sel
+        return activity_urling
+    else:
+        return bored
