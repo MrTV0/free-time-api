adfeb551f02a67efdecedb30349ce012ab615aaa
Taro Verschueren <57659923+MrTV0@users.noreply.github.com>
2022-11-18 15:30:24 +0100

    Update python-api.py

--- app/python-api.py
+++ app/python-api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from urllib.request import urlopen
 from pydantic import BaseModel
@@ -24,14 +24,17 @@ class Bored(BaseModel):
     participants_amount: int | None = None
 
 @app.post("/activity")
-async def create_item(bored: Bored):
+async def get_activity(bored: Bored):
     if bored.activity_sel:
         activity_urling = activity_url + "?type=" + bored.activity_sel
         if bored.participants_amount and bored.participants_amount > 0:
             activity_urlings = activity_urling + "&participants=" + str(bored.participants_amount)
             response = urlopen(activity_urlings)
             activity_json = json.loads(response.read())
-            return {"activity": activity_json.get("activity"), "type": activity_json.get("type"), "part_amount": activity_json.get("participants")}
+            if activity_json.get("activity") == None:
+                return {"activity": "No activity found"}
+            else:
+                return {"activity": activity_json.get("activity"), "type": activity_json.get("type"), "part_amount": activity_json.get("participants")}
         else:
             response = urlopen(activity_urling)
             activity_json = json.loads(response.read())
@@ -47,19 +50,40 @@ async def create_item(bored: Bored):
         return {"activity": activity_json.get("activity"), "type": activity_json.get("type"), "part_amount": activity_json.get("participants")}
 
 @app.get("/choice")
-async def get_random_percentage():
+async def get_choice():
     response = urlopen(yesno_url)
     yesno_json = json.loads(response.read())
     return {"text": yesno_json.get("answer")}
 
+@app.get("/yesnos/{amount}")
+async def get_yes_no(amount: int, max_amount: int):
+    strYesNo = ""
+    yes = 0
+    no = 0
+    if max_amount >= amount and amount > 0:
+        for i in range(amount):
+            response = urlopen(yesno_url)
+            yesno_json = json.loads(response.read())
+            yesno = yesno_json.get("answer")
+            strYesNo = strYesNo + yesno + " "
+            if yesno == "yes":
+                yes = yes + 1
+            else:
+                no = no + 1
+        if yes > no:
+            return {"fullString": strYesNo, "winner": "Yes wins over no!"}
+        elif yes == no:
+            return {"fullString": strYesNo, "winner": "It's a tie!"}
+        else:
+            return {"fullString": strYesNo, "winner": "No wins over yes!"}
+    else:
+        return {"fullString": "Invalid value!", "winner": "Please insert a number higher than 0 and lower than " + str(max_amount + 1)}
+
 @app.get("/break")
-async def get_random_percentage():
+async def get_coffee():
     return {"text": "Have a coffee!", "picture": "https://coffee.alexflipnote.dev/HrMCgJMACXc_coffee.png"}
 
-@app.post("/bored/")
-async def create_item(bored: Bored):
-    if bored.activity_sel:
-        activity_urling = activity_url + "?type=" + bored.activity_sel
-        return activity_urling
-    else:
-        return bored
+@app.get("/item/{item_id}")
+async def get_random_percentage(item_id: int, other_item: int | None = None):
+    total = item_id + other_item
+    return {"text": total}
